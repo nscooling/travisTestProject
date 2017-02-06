772d0284c8d6a7904290f2e588dcddd135de8d56
Niall Cooling <niall.cooling@gmail.com>
2017-02-06 16:53:37 +0000

    further budder tests

--- src/testProject.cpp
+++ src/testProject.cpp
@@ -17,3 +17,30 @@ TEST(buffer_test, test_initial_condition)
     ASSERT_EQ(true,buffer.isEmpty());
 }
 
+TEST(buffer_test, test_initial_put)
+{
+    Buffer buffer;
+    Buffer::Error err = buffer.add(1);
+
+    ASSERT_EQ(err,Buffer::OK);
+    ASSERT_EQ(false,buffer.isEmpty());
+}
+
+TEST(buffer_test, test_initial_get)
+{
+    Buffer buffer;
+    int data;
+    Buffer::Error err = buffer.get(data);
+    ASSERT_EQ(err,Buffer::EMPTY);
+}
+
+TEST(buffer_test, test_initial_putget)
+{
+    Buffer buffer;
+    Buffer::Error err = buffer.add(1);
+    int data;
+    err = buffer.get(data);
+    ASSERT_EQ(err,Buffer::OK);
+    ASSERT_EQ(true,buffer.isEmpty());
+    ASSERT_EQ(1,data);
+}
